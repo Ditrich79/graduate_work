abf1f7890ed3e8d4d928b605bea3247b188bd7bd
Ditrich79 <adk18@mail.ru>
2023-07-01 19:33:52 +0300

    010723

--- traveler/travel_lover/travels/urls.py
+++ traveler/travel_lover/travels/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.travels, name='travels'),
+    path('<int:travel_id>/', views.travel, name='travel'),
 ]
--- traveler/travel_lover/travels/views.py
+++ traveler/travel_lover/travels/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Travels
 
 
 def travels(request):
     travels = Travels.objects.all()
     return render(request, 'travels/travels.html', {'travels': travels})
+
+
+def travel(request, travel_id):
+    travel = get_object_or_404(Travels, pk=travel_id)
+    return render(request, 'travels/travel.html', {'travel': travel})
+
