b739684e4726d4ec7df420ca3b3bcee1b41983ff
Ditrich79 <adk18@mail.ru>
2023-09-18 20:30:14 +0300

    18.09.23

--- traveler/travel_lover/travels/views.py
+++ traveler/travel_lover/travels/views.py
@@ -1,29 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Travels
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def travels(request):
     travels = Travels.objects.all()
-
-    # page = request.GET.get('page')
-    # results = 3
-    # paginator = Paginator(travels, results)
-    #
-    # try:
-    #     travels = paginator.page(page)
-    # except PageNotAnInteger:
-    #     page = 1
-    #     travels = paginator.page(page)
-    # except EmptyPage:
-    #     page = paginator.num_pages
-    #     travels = paginator.page(page)
-    #
-    # context = {
-    #     {'travels': travels},
-    #     {'paginator': paginator},
-    # }
-
     return render(request, 'travels/travels.html', {'travels': travels})
 
 
